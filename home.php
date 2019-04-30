db0502b29efe8b8ac99f23cc094b19185e73b490
patwary88 <mainuddinpatwary1988@gmail.com>
2019-04-30 23:21:46 +0600

    add login page

---
+++ home.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <script src="resources/js/jquery-3.4.0.js"></script>
+    <script src="resources/js/jquery-3.4.0.slim.js"></script>
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    
+    <script src="resources/js/popper.min.js"></script>
+    <script src="resources/js/bootstrap.min.js"></script>
+  </body>
+</html>
